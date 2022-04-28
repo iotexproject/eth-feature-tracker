a715f5740744dec687d13779e6153bb0f1563449
Jeson <chenjie@imoocc.com>
2022-04-28 08:58:49 +0800

    change output format

--- eip_feature_tracker.py
+++ eip_feature_tracker.py
@@ -359,7 +359,7 @@ def export_data_to_md(eipurl):
     dict_file.write('\n## Ethereum Feature Tracker')
     dict_file.write('\n| I | O | T | X | %s updated|' % (gen_time))
     dict_file.write('\n | EIP Number | Release info | Commit info |  Block number | Iotex supported |')
-    dict_file.write('\n | :--- | :--- | :--- | :--- | :--- | :--- |')
+    dict_file.write('\n | :--- | :--- | :--- | :--- | :--- |')
     dict_file.close()
 
     dict_file = open(file_path, 'a', encoding='UTF-8')
